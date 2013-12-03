eadca09305f499cb8b95dae0fcda38caf0e5436f
HunterTidwell <wht1@rice.edu>
2013-12-03 17:42:33 -0600

    Made fall match the rest of the stubs

--- seasons/fall.py
+++ seasons/fall.py
@@ -29,10 +29,12 @@ RECRUIT_TIME = 10 * 1000
 FOLLOW_TIME = 10 * 1000
 
 def fall():
+    beh.init(0.22, 40, 0.5, 0.1)
 
     state = STATE_IDLE
 
     while True:
+        # run the system updates
         new_nbrs = beh.update()
         
         nbrList = neighbors.get_neighbors()
@@ -40,7 +42,7 @@ def fall():
             print nbrList
         beh_out = beh.BEH_INACTIVE
 
-        #FINITE STATE MACHINE OF DOOM
+        # this is the main finite-state machine
         if state = STATE_IDLE:
             leds.set_pattern('r', 'circle', LED_BRIGHTNESS)
             if rone.button_get_value('r'):
@@ -57,6 +59,21 @@ def fall():
             pass
         elif state = STATE_RETURN_TO_BASE_RECRUIT:
             pass
-        #END OF FINITE STATE MACHINE OF DOOM
+        # end of the FSM
+        bump_beh_out = beh.bump_beh(MOTION_TV)
+
+        beh_out = beh.subsume([beh_out, bump_beh_out])
+
+        # set the beh velocities
+        beh.motion_set(beh_out)
+
+        #set the HBA message
+        hba.set_msg(0, 0, 0)
+
+# Helper Functions
+
+
+
+# Start!
 
 fall()
