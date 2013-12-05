a7dc5c33a85288c658aa2375e8d0047b1324f5aa
MrScribbler <pdb3@rice.edu>
2013-12-04 19:21:58 -0600

    Wrote a function to get back to the nav tower
    It has a while true loop for testing. Will need to call the function
    multiple times.

--- examples/Nav_Tower_Motion.py
+++ examples/Nav_Tower_Motion.py
@@ -22,73 +22,87 @@ STATE_MOVE_FROM_TOWER = 1
 
 # Other constants
 LED_BRIGHTNESS = 40
-MOVE_TO_TOWER_DISTANCE = 500
+MOVE_TO_TOWER_DISTANCE = 3000
 #find_nav_tower_nbr()
 
 def nav_tower_motion():
     beh.init(0.22, 40, 0.5, 0.1)
-
     state = STATE_MOVE_TO_TOWER
     motion_start_odo = pose.get_odometer()
-
-    while True:
-        # run the system updates
-        new_nbrs = beh.update()
-        
-        #make a nbr list        
-        nbrList = neighbors.get_neighbors()
-        if new_nbrs:
-            print nbrList
-        beh_out = beh.BEH_INACTIVE
-            
+    while True: #while True loop just for testing, will need to call many times
         nav_tower = hba.find_nav_tower_nbr(127)
-        # this is the main finite-state machine
         if state == STATE_MOVE_TO_TOWER:
-            # Move towards the nav_tower until turning around distance reached
-            if nav_tower != None:
-                # move forward
+            new_nbrs = beh.update()
+        # Move towards the nav_tower until turning around distance reached
+            if nav_tower != None:      # move forward
                 beh_out = beh.follow_nbr(nav_tower, MOTION_TV)
                 leds.set_pattern('g', 'blink_fast', LED_BRIGHTNESS)
             else:
                 leds.set_pattern('g', 'circle', LED_BRIGHTNESS)
+                beh_out = beh.follow_nbr(nav_tower, MOTION_TV)  
+            distance_to_go = (motion_start_odo + MOVE_TO_TOWER_DISTANCE) - pose.get_odometer()
+            beh.motion_set(beh_out)
+            if distance_to_go < 0:    
+                state = STATE_IDLE   
+    return
+#     while True:
+#         # run the system updates
+#         new_nbrs = beh.update()
+        
+#         #make a nbr list        
+#         nbrList = neighbors.get_neighbors()
+#         if new_nbrs:
+#             print nbrList
+#         beh_out = beh.BEH_INACTIVE
+            
+#         nav_tower = hba.find_nav_tower_nbr(127)
+#         # this is the main finite-state machine
+#         if state == STATE_MOVE_TO_TOWER:
+#             # Move towards the nav_tower until turning around distance reached
+#             if nav_tower != None:
+#                 # move forward
+#                 beh_out = beh.follow_nbr(nav_tower, MOTION_TV)
+#                 leds.set_pattern('g', 'blink_fast', LED_BRIGHTNESS)
+#             else:
+#                 leds.set_pattern('g', 'circle', LED_BRIGHTNESS)
                 
-            # stop after a fixed distance_to_go
-            distance_to_go = (motion_start_odo + MOVE_TO_TOWER_DISTANCE) - pose.get_odometer() 
-            if distance_to_go < 0:
-                motion_start_odo = pose.get_odometer()
-                state = STATE_MOVE_FROM_TOWER
+#             # stop after a fixed distance_to_go
+#             distance_to_go = (motion_start_odo + MOVE_TO_TOWER_DISTANCE) - pose.get_odometer() 
+#             if distance_to_go < 0:
+#                 motion_start_odo = pose.get_odometer()
+#                 state = STATE_MOVE_FROM_TOWER
 
-            if new_nbrs:
-                print "move to tower: dist=", distance_to_go
+#             if new_nbrs:
+#                 print "move to tower: dist=", distance_to_go
 
-        elif state == STATE_MOVE_FROM_TOWER:
-            # Move away from the nav_tower until turning around distance reached
-            if nav_tower != None:
-                # move forward
-                beh_out = beh.avoid_nbr(nav_tower, MOTION_TV)
-                leds.set_pattern('r', 'blink_fast', LED_BRIGHTNESS)
-            else:
-                leds.set_pattern('r', 'circle', LED_BRIGHTNESS)
+#         elif state == STATE_MOVE_FROM_TOWER:
+#             # Move away from the nav_tower until turning around distance reached
+#             if nav_tower != None:
+#                 # move forward
+#                 beh_out = beh.avoid_nbr(nav_tower, MOTION_TV)
+#                 leds.set_pattern('r', 'blink_fast', LED_BRIGHTNESS)
+#             else:
+#                 leds.set_pattern('r', 'circle', LED_BRIGHTNESS)
                 
-            # stop after a fixed distance_to_go
-            distance_to_go = (motion_start_odo + MOVE_TO_TOWER_DISTANCE) - pose.get_odometer() 
-            if distance_to_go < 0:
-                motion_start_odo = pose.get_odometer()
-                state = STATE_MOVE_TO_TOWER
+#             # stop after a fixed distance_to_go
+#             distance_to_go = (motion_start_odo + MOVE_TO_TOWER_DISTANCE) - pose.get_odometer() 
+#             if distance_to_go < 0:
+#                 motion_start_odo = pose.get_odometer()
+#                 state = STATE_MOVE_TO_TOWER
 
-            if new_nbrs:
-                print "move from tower: dist=", distance_to_go
+#             if new_nbrs:
+#                 print "move from tower: dist=", distance_to_go
                 
-        # end of the FSM
-        bump_beh_out = beh.bump_beh(MOTION_TV)
+#         # end of the FSM
+#         bump_beh_out = beh.bump_beh(MOTION_TV)
 
-        beh_out = beh.subsume([beh_out, bump_beh_out])
+#         beh_out = beh.subsume([beh_out, bump_beh_out])
 
-        # set the beh velocities
-        beh.motion_set(beh_out)
+#         # set the beh velocities
+#         beh.motion_set(beh_out)
 
-        #set the HBA message
-        hba.set_msg(0, 0, 0)
+#         #set the HBA message
+#         hba.set_msg(0, 0, 0)
 
             
 nav_tower_motion()
