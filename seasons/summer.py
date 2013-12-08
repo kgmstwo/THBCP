010707859b421afcd48487ccc0597f069fde49c1
kgmstwo <kgmstwo@gmail.com>
2013-12-08 15:10:18 -0600

    wrote summer to a basic point

--- seasons/summer.py
+++ seasons/summer.py
@@ -15,6 +15,10 @@ MOTION_TV = 100
 
 # FSM States
 STATE_IDLE = 0
+STATE_FIND_QUEEN = 1
+STATE_BUMP_QUEEN = 2
+STATE_RETURN = 3
+STATE_QUEEN = 4
 
 # Other constants
 LED_BRIGHTNESS = 40
@@ -27,33 +31,73 @@ def summer():
     while True:
         # run the system updates
         new_nbrs = beh.update()
-        
+
         nbrList = neighbors.get_neighbors()
         if new_nbrs:
             print nbrList
         beh_out = beh.BEH_INACTIVE
-            
+
         # this is the main finite-state machine
         if state == STATE_IDLE:
             leds.set_pattern('r', 'circle', LED_BRIGHTNESS)
+            leds.set_pattern('b', 'circle', LED_BRIGHTNESS)
             if rone.button_get_value('r'):
-                state = STATE_MOVE_TO_FLOWER
+                state = STATE_FIND_QUEEN
+            if rone.button_get_value('b'):
+                state = STATE_QUEEN
             if new_nbrs:
                 print "idle"
+        elif state == STATE_FIND_QUEEN:
+            beh_out = (MOTION_TV, 0)
+            queen = get_queen()
+            if not queen == None:
+                state = STATE_BUMP_QUEEN
+            else:
+                #go straight and hope for the best
+                beh_out = beh.tvrv(MOTION_TV, MOTION_RV)
+        elif state == STATE_BUMP_QUEEN:
+            queen = get_queen()
+            if queen == None:
+                state = STATE_RETURN
+            else:
+                if (neighbors.get_nbr_range_bits(queen) > 6) or (beh.bump_angle_get() != None):
+                    state = STATE_RETURN
+                else:
+                    beh_out = beh.follow_nbr(queen, MOTION_TV)
+        elif state == STATE_RETURN:
+            if rone.button_get_value('r'):
+                state = STATE_FIND_QUEEN
+
+            queen = get_queen()
+            if queen == None:
+                state = STATE_IDLE
+            else:
+                beh_out = beh.avoid_nbr(queen, MOTION_TV)
+        elif state == STATE_QUEEN:
+            if new_nbrs:
+                print 'Ich bin die Koenigin!'
 
         # end of the FSM
         bump_beh_out = beh.bump_beh(MOTION_TV)
-
         beh_out = beh.subsume([beh_out, bump_beh_out])
+
 
         # set the beh velocities
         beh.motion_set(beh_out)
 
         #set the HBA message
-        hba.set_msg(0, 0, 0)
+        msg = [0,0,0]
+        msg[MSG_STATE] = state
+        hba.set_msg(msg)
 
 # Helper functions
-
+def get_queen():
+    nbr_list = hba.get_robot_neighbors()
+    for nbr in nbr_list:
+        is_queen = hba.get_msg_from_nbr(nbr, new_nbrs)[MSG_STATE] == STATE_QUEEN
+        if is_queen:
+            return nbr
+    return None
 
 
 # Start!
