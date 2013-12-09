eab408b8d7bc7308324c736227268515d8f56d23
HunterTidwell <wht1@rice.edu>
2013-12-09 12:43:51 -0600

    try, then try again

--- seasons/winter.py
+++ seasons/winter.py
@@ -20,12 +20,12 @@ STATE_LIGHT = 2
 STATE_EDGE_OF_LIGHT = 3
 
 # MSG components
-MSG_POS_IN_LIGHT = 0
+MSG_POS_STATE = 0
 
 # Other constants
 LED_BRIGHTNESS = 40
-CLOSENESS_CONSTANT = 4
-BRIGHTNESS_CONSTANT = 300
+CLOSENESS_CONSTANT = 5
+BRIGHTNESS_CONSTANTS = {'fl':300, 'fr':300, 'b':300}
 
 def winter():
     global new_nbrs
@@ -49,11 +49,9 @@ def winter():
                 ##### This is one way to find a cutoff for being in light.
                 ##### Make sure you press the 'r' button when the robot is
                 ##### in the light!
-                global BRIGHTNESS_CONSTANT
-                brightnesses = [rone.light_sensor_get_value(dir) for \
-                                dir in ['fl', 'fr', 'r']
-                                ]
-                BRIGHTNESS_CONSTANT = 0.8 * max(brightnesses)
+                global BRIGHTNESS_CONSTANTS
+                for sensor_dir in BRIGHTNESS_CONSTANTS.keys():
+                    BRIGHTNESS_CONSTANT[sensor_dir] = 0.75 * rone.light_sensor_get_value(sensor_dir)
                 #####
                 initial_time = sys.time()
                 state = STATE_LIGHT
@@ -98,10 +96,7 @@ def winter():
 
         #set the HBA message
         msg = [0, 0, 0]
-        if self_in_light():
-            msg[MSG_POS_IN_LIGHT] = 1
-        else:
-            msg[MSG_POS_IN_LIGHT] = 0
+        msg[MSG_POS_STATE] = state
         hba.set_msg(msg[0], msg[1], msg[2])
 
 # Helper functions
@@ -111,8 +106,8 @@ def get_nbrs_in_light():
     nbr_list = hba.get_robot_neighbors()
     nbrs_in_light = []
     for nbr in nbr_list:
-        in_light = hba.get_msg_from_nbr(nbr, new_nbrs)[MSG_POS_IN_LIGHT]
-        if in_light:
+        state = hba.get_msg_from_nbr(nbr, new_nbrs)[MSG_POS_STATE]
+        if state == STATE_LIGHT:
             nbrs_in_light.append(nbr)
     return nbrs_in_light
 
@@ -121,8 +116,8 @@ def get_nbrs_in_dark():
     nbr_list = hba.get_robot_neighbors()
     nbrs_in_dark = []
     for nbr in nbr_list:
-        in_light = hba.get_msg_from_nbr(nbr, new_nbrs)[MSG_POS_IN_LIGHT]
-        if not in_light:
+        state = hba.get_msg_from_nbr(nbr, new_nbrs)[MSG_POS_STATE]
+        if state == STATE_DARK:
             nbrs_in_dark.append(nbr)
     return nbrs_in_dark
 
@@ -153,24 +148,24 @@ def move_in_dir(bearing):
     bearing = math2.normalize_angle(bearing)
     if bearing >= 0:
         if bearing > math.pi / 2:
-            tv = int(MOTION_TV * (1 - bearing * 2 / math.pi))
-            rv = int(MOTION_RV * (2 - bearing * 2 / math.pi))
+            tv = int(-MOTION_TV * (-1.0 + bearing * 2.0 / math.pi))
+            rv = int(-MOTION_RV * (2.0 - bearing * 2.0 / math.pi))
         else:
-            tv = int(MOTION_TV * (-1 + bearing * 2 / math.pi))
-            rv = int(MOTION_RV * (-2 + bearing * 2 / math.pi))
+            tv = int(MOTION_TV * (1.0 - bearing * 2.0 / math.pi))
+            rv = int(MOTION_RV * (bearing * 2.0 / math.pi))
     else:
         if bearing < -math.pi / 2:
-            tv = int(MOTION_TV * (1 + bearing * 2 / math.pi))
-            rv = int(MOTION_RV * (2 + bearing * 2 / math.pi))
+            tv = int(-MOTION_TV * (-1.0 - bearing * 2.0 / math.pi))
+            rv = int(MOTION_RV * (2.0 + bearing * 2.0 / math.pi))
         else:
-            tv = int(MOTION_TV * (-1 - bearing * 2 / math.pi))
-            rv = int(MOTION_RV * (-2 - bearing * 2 / math.pi))
-    return tv, rv        
+            tv = int(MOTION_TV * (1.0 + bearing * 2.0 / math.pi))
+            rv = int(-MOTION_RV * (-bearing * 2.0 / math.pi))
+    return tv, rv
 
 def self_in_light():
     any_in_light = False
-    for sensor_dir in ['fr', 'fl', 'r']:
-        this_one_in_light = (rone.light_sensor_get_value(sensor_dir) > BRIGHTNESS_CONSTANT)
+    for sensor_dir in BRIGHTNESS_CONSTANTS.keys():
+        this_one_in_light = (rone.light_sensor_get_value(sensor_dir) > BRIGHTNESS_CONSTANTS[sensor_dir])
         any_in_light = any_in_light or this_one_in_light
     return any_in_light
 
